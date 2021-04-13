ac52af8f929f1e8af9ec2e53897434de8e42a16b
Nildo Silva <nildotoni@gmail.com>
2021-04-13 00:49:28 -0300

    Adicionado errorHandler Global

---
+++ src/main/java/br/com/zupacademy/antoniomatias/casadocodigo/errorhandlers/FieldErrorOutputDto.java
@@ -0,0 +1,25 @@
+package br.com.zupacademy.antoniomatias.casadocodigo.errorhandlers;
+
+public class FieldErrorOutputDto {
+	private String field;
+	private String message;
+	
+	FieldErrorOutputDto(){}
+
+	
+	public FieldErrorOutputDto(String field, String message) {
+		this.field = field;
+		this.message = message;
+	}
+
+
+	public String getField() {
+		return field;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+	
+	
+}
